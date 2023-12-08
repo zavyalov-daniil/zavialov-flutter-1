e1acba9e2a03829c8cb32ff0ae7599a4689a4aa9
dezavv1 <dezavv1@gmail.com>
2023-12-08 21:23:37 +0600

    [+] add appbar button

--- lib/main.dart
+++ lib/main.dart
@@ -73,8 +73,57 @@ class MyHomePage extends StatelessWidget {
                 ),
               ]),
             ),
-
           ]),
+          Padding(
+            padding: const EdgeInsets.only(top: 14),
+            child: SizedBox(
+              height: 56,
+              width: MediaQuery.of(context).size.width,
+              child: Container(
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    Expanded(
+                      child: Container(
+                        decoration: const BoxDecoration(
+                          border: Border(
+                            bottom: BorderSide(width: 2, color: Colors.green),
+                          ),
+                        ),
+                        child: TextButton(
+                          onPressed: () {
+                            //
+                          },
+                          child: const Text(
+                            'Профиль',
+                            style: TextStyle(color: Colors.black),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(
+                        decoration: const BoxDecoration(
+                          border: Border(
+                            bottom: BorderSide(width: 1, color: Colors.grey),
+                          ),
+                        ),
+                        child: TextButton(
+                          onPressed: () {
+                            //
+                          },
+                          child: const Text(
+                            'Настройки',
+                            style: TextStyle(color: Colors.black),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );
