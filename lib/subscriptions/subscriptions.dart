79cad91aecbfd7b203d8bf77b6b3c664a8ffc3c2
dezavv1 <dezavv1@gmail.com>
2023-12-08 23:34:50 +0600

    [+] add subscription card

--- lib/subscriptions/subscriptions.dart
+++ lib/subscriptions/subscriptions.dart
@@ -1,28 +1,30 @@
 import 'package:flutter/cupertino.dart';
+import 'subscription_card.dart';
 
 Widget subscriptions(BuildContext context) {
-  return const Padding(
+  return Padding(
       padding: EdgeInsets.only(left: 16, top: 30, bottom: 20, right: 16),
       child: Column(
         children: [
-          Text('У вас подключено',
+          const Text('У вас подключено',
               style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.w700,
-                  fontFamily: "SF Pro Display",
-                  letterSpacing: -0.7,
-                )
-          ),
-          SizedBox(height: 8),
-          Text('Подписки, автоплатежи и сервисы на которые вы подписались',
+                fontSize: 20,
+                fontWeight: FontWeight.w700,
+                fontFamily: "SF Pro Display",
+                letterSpacing: -0.7,
+              )),
+          const SizedBox(height: 8),
+          const Text(
+              'Подписки, автоплатежи и сервисы на которые вы подписались',
               style: TextStyle(
-                  fontSize: 14,
-                  color: Color(0x8C000000),
-                  fontFamily: "SF Pro Display",
-                  letterSpacing: -0.7,
-                  )
-          ),
+                fontSize: 14,
+                color: Color(0x8C000000),
+                fontWeight: FontWeight.w500,
+                fontFamily: "SF Pro Display",
+                letterSpacing: -0.42,
+              )),
+          subscriptionCard(context, 'assets/images/sber.png', 'СберПрайм',
+              'Платёж 9 июля', '199 ₽ в месяц'),
         ],
-      )
-  );
+      ));
 }
